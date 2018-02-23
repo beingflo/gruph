f08898f942a6a2af47d3ed14c091f4612fb2e342
Florian Marending <marending.florian@gmail.com>
2018-02-23 22:14:39 +0100

    Add bfs

---
+++ src/bfs.rs
@@ -0,0 +1,69 @@
+use std::collections::VecDeque;
+use Graph;
+use Node;
+
+pub fn breadth_first_search<'a, T: Graph<'a>>(graph: &'a T, start: Node) -> Vec<Option<Node>> {
+    let mut q = VecDeque::new();
+    let mut pred = vec![None; graph.num_nodes()];
+
+    q.push_front(start);
+    pred[start] = Some(start);
+
+    while !q.is_empty() {
+        let v = q.pop_back().unwrap();
+        for u in graph.neighbors(v) {
+            if pred[u] == None {
+                pred[u] = Some(v);
+                q.push_front(u);
+            }
+        }
+    }
+
+    pred
+}
+
+#[cfg(test)]
+mod tests {
+    use Graph;
+    use EdgeList;
+    use breadth_first_search;
+
+    #[test]
+    fn simple() {
+        let mut graph = EdgeList::new();
+        graph.add_edge(0,1);
+        graph.add_edge(1,2);
+
+        assert_eq!(breadth_first_search(&graph, 0), vec![Some(0), Some(0), Some(1)]);
+    }
+
+    #[test]
+    fn complete_graph() {
+        let mut graph = EdgeList::new();
+
+        for u in 0..100 {
+            for v in 0..100 {
+                graph.add_edge(u,v);
+            }
+        }
+
+        assert_eq!(breadth_first_search(&graph, 0), vec![Some(0); graph.num_nodes()]);
+    }
+
+    #[test]
+    fn line_graph() {
+        let mut graph = EdgeList::new();
+
+        for u in 0..100 {
+            graph.add_edge(u,u+1);
+        }
+
+        let mut pred = vec![None; graph.num_nodes()];
+        pred[0] = Some(0);
+        for u in 1..101 {
+            pred[u] = Some(u-1);
+        }
+
+        assert_eq!(breadth_first_search(&graph, 0), pred);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,12 @@
 mod graph;
 mod edge_list;
+mod bfs;
 
+// Data structues
 pub use graph::Graph;
+pub use graph::Node;
 pub use edge_list::EdgeList;
+
+
+// Algorithms
+pub use bfs::breadth_first_search;
